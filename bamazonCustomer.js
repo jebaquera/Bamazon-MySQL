d85e0dab5827bbbdc4de9df8fd19bf341c0dd820
jebaquera <jebaquera@gmail.com>
2019-06-29 10:48:18 -0500

    added js to display results as a table using cli-table.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,44 @@
-// five - Running this application will first display all of the items available for sale: inlcude the id's, names, prices, 
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var Table = require("cli-table");
 
+// Connection information for the sql database
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "dolly_19",
+  database: "bamazon_db"
+});
 
+// Connect to the mysql server and sql database
+connection.connect(function(err) {
+  if (err) throw err;
+  // run the displayPeroducts function after the connection is made to display the items to the user
+  displayProducts();
+});
+
+// five - Running this application will first display all of the items available for sale: include the id's, names, prices, 
+var displayProducts = function(){
+	var query = "Select * FROM products";
+	connection.query(query, function(err, res){
+		if(err) throw err;
+		var displayTable = new Table ({
+			head: ["Item ID#", "Product Name", "Department Name", "Price ($)", "In-Stock"],
+			colWidths: [10,45,20,15,10]
+		});
+		for(var i = 0; i < res.length; i++){
+			displayTable.push(
+				[res[i].item_id,res[i].product_name, res[i].department_name, res[i].retail_price, res[i].stock_quantity]
+				);
+    }
+		console.log(displayTable.toString());
+		// purchasePrompt();
+  });
+  // logs the actual query being run
+  console.log(query.sql);
+  connection.end();
+}
 
 // six - The app should then prompt users with two messages:
 // item_id
